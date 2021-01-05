84608df980fed63fdd49c5e04b7e44506bfe7ffb
openeyes-lab <74488113+openeyes-lab@users.noreply.github.com>
2021-01-05 21:17:00 +0100

    Update sd108.c
    documentation change

--- build/sd108.c
+++ build/sd108.c
@@ -1,3 +1,30 @@
+/*
+ * sd108.c - Part of OPEN-EYES-II products, Linux kernel modules for GPIO 
+ * expansion.
+ * This driver handles the SD108 FW running on ATTINY817.
+ * Author:
+ * Massimiliano Negretti <massimiliano.negretti@open-eyes.it> 2020-07-12
+ *
+ * This driver enable 3 different feature:
+ * 1) GPIO
+ * 2) PWM
+ * 3) LCD BACKLIGHT
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/acpi.h>
