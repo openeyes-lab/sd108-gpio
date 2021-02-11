a9db35d7d33213523014a0cfa33acb9a212d4bf0
openeyes-lab <74488113+openeyes-lab@users.noreply.github.com>
2021-02-11 11:23:40 +0100

    Update sd108.c
    copyright fix

--- build/sd108.c
+++ build/sd108.c
@@ -10,9 +10,14 @@
  * 2) PWM
  * 3) LCD BACKLIGHT
  *
+ * This file is part of sd108-gpio distribution
+ * https://github.com/openeyes-lab/sd108-gpio
+ *
+ * Copyright (c) 2021 OPEN-EYES Srl 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
